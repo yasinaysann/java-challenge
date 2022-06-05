e264e2bc42227049e7eda648aa883227d21e861d
Yasin Aysan <yasinaysann@gmail.com>
2022-06-05 23:04:58 +0300

    FilterApiImpl.java filtering has been done

--- src/main/java/com/kodgemisi/filter/FilterApiImpl.java
+++ src/main/java/com/kodgemisi/filter/FilterApiImpl.java
@@ -4,6 +4,7 @@ import com.kodgemisi.usermanagement.User;
 import com.kodgemisi.usermanagement.UserService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilterApiImpl implements FilterApi {
 
@@ -17,13 +18,23 @@ public class FilterApiImpl implements FilterApi {
 	public List<User> unverifiedUnder18() {
 		//FIXME currently returns all the users unfiltered, you should fix this method
 		// If you are not sure how to implement this method, please refer to the Javadoc or the FilterApi interface
-		return userService.list();
+		return userService.list().stream()
+				.filter(user -> user.getAge() < 18 && isVerified(user) == false)
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public List<User> verifiedWithTrPrimaryPhone() {
 		//FIXME currently returns all the users unfiltered, you should fix this method
 		// If you are not sure how to implement this method, please refer to the Javadoc or the FilterApi interface
-		return userService.list();
+		return userService.list().stream()
+				.filter(user -> isVerified(user) && user.getProfile()
+						.getPrimaryPhone()
+						.number().substring(0,3).equals("+90"))
+				.collect(Collectors.toList());
+	}
+
+	private boolean isVerified(User user){
+		return user.isVerified();
 	}
 }
