158201896ffa6315baa4643b50f4b48ba0405b19
Yasin Aysan <yasinaysann@gmail.com>
2022-06-05 22:52:15 +0300

    phone number validation add

--- src/main/java/com/kodgemisi/usermanagement/Phone.java
+++ src/main/java/com/kodgemisi/usermanagement/Phone.java
@@ -8,10 +8,27 @@ public class Phone implements CharSequence {
 	private final String phoneNumber;
 
 	public Phone(String phoneNumber) {
-		this.phoneNumber = phoneNumber == null ? "" : phoneNumber;
+		this.phoneNumber = checkPhoneNumber(phoneNumber) == null ? "" : phoneNumber;
 	}
 
 	public String number() {
+		return phoneNumber;
+	}
+
+	public String checkPhoneNumber(String phoneNumber){
+		if (phoneNumber == null)
+			throw new IllegalArgumentException("A phone number cannot be null");
+		if (phoneNumber.isBlank())
+			throw new IllegalArgumentException("A phone number cannot be empty");
+		if(phoneNumber.contentEquals("112") || phoneNumber.contentEquals("911")){
+			return phoneNumber;
+		}
+		if(phoneNumber.charAt(0) != '+')
+			throw new IllegalArgumentException("may not start with `+` if it's `112` or `911`");
+		if(phoneNumber.length() < 5)
+			throw new IllegalArgumentException("Less than 5 characters");
+		if(phoneNumber.substring(0,3).contentEquals("+90") && phoneNumber.length() != 13)
+			throw new IllegalArgumentException("should be exactly 13-char long when it starts with `+90`");
 		return phoneNumber;
 	}
 
